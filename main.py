7d5dec3c524d958d4e5fbeeb83bb32fb1c76f010
Prahadeeswaran <104518977+Prahadeeswara@users.noreply.github.com>
2024-08-30 23:45:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from flask import Flask, render_template, request, session, redirect, url_for
 from flask_socketio import join_room, leave_room, send, SocketIO
 import random
 from string import ascii_uppercase
-from gevent import monkey
-monkey.patch_all()
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "hjhjsdahhds"
